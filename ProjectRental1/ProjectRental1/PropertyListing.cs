2451b13182dacae92add5cf6fdd54c53a0cd125d
unknown <sneha.lalita22@gmail.com>
2021-09-20 17:57:47 +0530

    day6 serialization

---
+++ ProjectRental1/ProjectRental1/PropertyListing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectRental1
+{
+    
+        [Serializable]
+        public class PropertyListing
+        {
+            private Property[] prop = new Property[100];
+
+            public Property this[int i]
+            {
+                get { return prop[i]; }
+                set { prop[i] = value; }
+            }
+        }
+    
+}
