817257a9785eba22081014ecd03d647f423c303a
unknown <sneha.lalita22@gmail.com>
2021-09-24 00:21:14 +0530

    Day 4

---
+++ Day4/DelegateDemo/DelegateDemo/Delegate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    public delegate int mydel(int num1, int num2);
+    class Delegate
+    {
+    }
+}
---
+++ Day4/DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+        static int sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            mydel mydelegate = new mydel(sum);
+            int res = mydelegate(3, 5);
+            Console.WriteLine(res);
+
+            Console.Read();
+
+        }
+    }
+}
